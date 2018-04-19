82dcc13411e95260579e90f424a2fa584ca301e0
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-19 19:26:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(talk)
   if talk == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
-  elsif talk.upcase
+  elsif talk == talk.upcase
       "NO, NOT SINCE 1938!"
   else
      "HUH?! SPEAK UP, SONNY!"
