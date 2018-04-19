0d7dd78b58e4361ae35a3cea7a4a89dcb1f460b5
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-19 19:13:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string == "I LOVE YOU GRANDMA"
+  if string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif string.uppercase
+  elsif string.upcase
     puts "NO, NOT SINCE 1938!"
-  elsif !string.uppercase
+  elsif !string.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
